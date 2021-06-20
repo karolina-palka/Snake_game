56c9176f759f27e23bdfeeb3dcf6e0a85048c019
Karolina Pałka <karolina.palka.programista@gmail.com>
2021-06-20 22:23:04 +0200

    Create key-release events

--- main.py
+++ main.py
@@ -14,9 +14,17 @@ screen.tracer(0)
 
 snake = Snake()
 
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.right, "Right")
+screen.onkey(snake.left, "Left")
+
 is_game_on = True
 
 while is_game_on:
     screen.update()
     time.sleep(0.1)
     snake.move()
+    #if (snake.get_snake_head_poisiton()== [food.x_index, food.y_index]):
+    
